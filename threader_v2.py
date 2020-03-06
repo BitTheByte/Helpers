26b2762b99b0df0d7b364a8235e6dc6d73a2a453
Ahmed Ezzat <ahmed.ezzat119@outlook.com>
2020-03-06 10:11:32 +0200

    Removed: locks due to performance issues

--- threader_v2.py
+++ threader_v2.py
@@ -4,22 +4,27 @@ import time
 import signal
 import sys
 import types
+import sys
 
 class Channel(object):
     def __init__(self,name='default'):
         self.name = name
         self.__stop = False
-        self.__lock = threading.Lock()
         self.__items = []
+        signal.signal(signal.SIGINT, self.signal_handler)
+
+    def signal_handler(self,sig,frame):
+        self.__stop = True
+        sys.exit(0)
 
     def append(self,item):
-        with self.__lock:
-            self.__items.append(item)
+        self.__items.append(item)
 
     def pop(self):
-        with self.__lock:
-            return self.__items.pop(0)
-    
+        if len(self.__items) > 0:
+            return True, self.__items.pop(0)
+        return False, None
+
     def __str__(self):
         return str(self.__items)
 
@@ -31,7 +36,7 @@ class Channel(object):
 
     def wait(self):
         while len(self.__items)  > 0:
-            time.sleep(0.01)
+            time.sleep(0.1)
 
     def close(self):
         self.__stop = True
@@ -39,38 +44,34 @@ class Channel(object):
 
 result = namedtuple("Result","func ret args channel wid")
 
-def _worker(wid,target,channel,lock,callback=None):
-    while(  channel.open() ):
-        if len(channel) > 0:
-            args   = channel.pop()
-            res    = target(args)
+def _worker(wid,target,channel,callback=None):
+    while( channel.open() ):
+            ok, args = channel.pop()
+            if not ok: time.sleep(0.50); continue
 
-            with lock: 
-                if type(callback) == types.FunctionType:
-                    callback(result(wid= wid, channel= channel,
+            if type(callback) == types.FunctionType:
+                callback(result(wid= wid, channel= channel,
                             func    = target,
                             args    = args,
-                            ret     = res,
+                            ret     = target(args),
                         ))
-        else:
-            time.sleep(0.01)
 
 def workers(target,channel,count=5,callback=None):
-    lock = threading.Lock()
     for _id in range(1,count+1):
-        threading.Thread(target=_worker,args=(_id,target,channel,lock,callback,)).start()
+        threading.Thread(target=_worker,args=(_id,target,channel,callback,)).start()
 
 
 # Example 
 mychannel = Channel() # Channel for sending function arguments 
 
 def hello_world(num):
+    time.sleep(2)
     return num * 5
 
 def on_finish(result):
     ret = result.ret
     arg = result.args
-    print("Hello(%s) returned: %s" % (arg,ret))
+    print("Hello(%s) returned: %s\n" % (arg,ret),end='')
 
 mychannel.append(10)       # channels could be initialized before workers
 mychannel.append(20)       # channels could be initialized before workers                                                                  
@@ -78,9 +79,9 @@ mychannel.append(20)       # channels could be initialized before workers
 workers(                                                                  
     target   = hello_world,    # function pointer
     channel  = mychannel,      # array which contains arguments                                           
-    count    = 5,              # number of simultaneous runs of the target function
+    count    = 2,              # number of simultaneous runs of the target function
     callback = on_finish       # callback funtion which get executed on every target function finished                                               
-)                                                                  
+)        
                                                                   
 mychannel.append(30)      # channels could be populated while the workers are running
 mychannel.append(40)      # channels could be populated while the workers are running

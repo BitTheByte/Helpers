4f91fc878abc202b764f3cb738f98390a125290b
Ahmed Ezzat <ahmed.ezzat119@outlook.com>
2020-03-06 08:49:49 +0200

    Added locks to prevent race conditions

--- threader_v2.py
+++ threader_v2.py
@@ -31,36 +31,34 @@ class Channel(object):
 
     def wait(self):
         while len(self.__items)  > 0:
-            time.sleep(0.1)
+            time.sleep(0.01)
 
     def close(self):
         self.__stop = True
 
 
 result = namedtuple("Result","func ret args channel wid")
-def _worker(wid,target,channel,lock,callback=None):
 
+def _worker(wid,target,channel,lock,callback=None):
     while(  channel.open() ):
         if len(channel) > 0:
             args   = channel.pop()
-            result = target(args)
+            res    = target(args)
 
             with lock: 
                 if type(callback) == types.FunctionType:
                     callback(result(wid= wid, channel= channel,
                             func    = target,
                             args    = args,
-                            ret     = result,
+                            ret     = res,
                         ))
         else:
-            time.sleep(0.1)
+            time.sleep(0.01)
 
 def workers(target,channel,count=5,callback=None):
     lock = threading.Lock()
     for _id in range(1,count+1):
         threading.Thread(target=_worker,args=(_id,target,channel,lock,callback,)).start()
-
-
 
 
 # Example 

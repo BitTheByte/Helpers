81c7a46d898673fb97e98467cdbbfcb52ce6f9a5
Ahmed Ezzat <ahmed.ezzat119@outlook.com>
2020-03-15 00:27:17 +0200

    Update threader_v2.py

--- threader_v2.py
+++ threader_v2.py
@@ -47,13 +47,17 @@ class Channel(object):
 
 result = namedtuple("Result","func ret args channel wid")
 
-def _worker(wid,target,channel,callback=None):
+def _worker(wid,target,channel,lock,callback=None):
     while( channel.open() ):
             ok, args = channel.pop()
             if not ok: time.sleep(0.50); continue
 
-            result = target(*args)
-            channel._jobs -= 1
+            try:
+                result = target(*args)
+                with lock: channel._jobs -= 1
+            except Exception as e:
+                with lock: channel._jobs -= 1
+                print(e)
 
             if type(callback) == types.FunctionType:
                 callback(result(wid= wid, channel= channel,
@@ -63,5 +67,6 @@ def _worker(wid,target,channel,callback=None):
                         ))
 
 def workers(target,channel,count=5,callback=None):
+    lock = threading.Lock()
     for _id in range(1,count+1):
-        threading.Thread(target=_worker,args=(_id,target,channel,callback,)).start()
+        threading.Thread(target=_worker,args=(_id,target,channel,lock,callback,)).start()

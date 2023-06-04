27f3ae7a91f71bbc392518465c3839470dbc7a2a
Sourcery AI <>
2023-06-04 18:53:05 +0000

    'Refactored by Sourcery'

--- ida-xor.py
+++ ida-xor.py
@@ -1,7 +1,7 @@
 #From :https://github.com/tq2ctf/writeups/tree/master/2018_06_23_GoogleCTF/keygenme
 
 def XorData(start,key,length):
-	print("[*] XOR start: {}".format(hex(start)))
+	print(f"[*] XOR start: {hex(start)}")
 	for ptr in range(start, start+length, 8): 
 	    PatchQword(ptr, Qword(ptr) ^ key);
 	Message("[*] XOR done :)\n")
